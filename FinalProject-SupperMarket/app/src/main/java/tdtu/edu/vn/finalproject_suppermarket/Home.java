652bb658fc9180ef6d4a7e2c72e77dbbecbbfa87
NguyenHuuAn <huuan04112002@gmail.com>
2023-05-04 05:44:30 +0700

    feat: change beetwen framents

--- FinalProject-SupperMarket/app/src/main/java/tdtu/edu/vn/finalproject_suppermarket/Home.java
+++ FinalProject-SupperMarket/app/src/main/java/tdtu/edu/vn/finalproject_suppermarket/Home.java
@@ -1,26 +1,55 @@
 package tdtu.edu.vn.finalproject_suppermarket;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import tdtu.edu.vn.finalproject_suppermarket.Products.DisplayMainProduct;
 
 public class Home extends AppCompatActivity {
+    BottomNavigationView navbar;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        navbar = findViewById(R.id.navBar);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
         DisplayMainProduct home = new DisplayMainProduct();
         fragmentTransaction.add(R.id.mainDisplay, home);
+        fragmentTransaction.commit();
 
-        // Commit the transaction
+        navbar.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.userAccount:
+                        addNewFragment(new UserInformation());
+                        break;
+                    case R.id.homepage:
+                        addNewFragment(new DisplayMainProduct());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void addNewFragment(Fragment fragment) {
+        fragmentManager.popBackStack();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainDisplay, fragment);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 }
