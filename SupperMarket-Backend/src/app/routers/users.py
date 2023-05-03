d8c53b88dfc630689645d0192f69b5cabce8b66c
NguyenHuuAn <huuan04112002@gmail.com>
2023-05-03 18:58:59 +0700

    feat: add get user's information

--- SupperMarket-Backend/src/app/routers/users.py
+++ SupperMarket-Backend/src/app/routers/users.py
@@ -13,7 +13,7 @@ router = APIRouter(
 
 def is_exists_user(username: str):
     is_exist_user_query = {"username": username}
-    is_exist_user = user_collection.find(is_exist_user_query, {"_id": 0})
+    is_exist_user = user_collection.find_one(is_exist_user_query, {"_id": 0})
     return is_exist_user != None
 
 
@@ -32,6 +32,17 @@ async def login(username: str, password: str):
         return {"status:": False, "message": "Wrong username/password"}
 
     return {"status:": True, "message": "Login successfully"}
+
+
+@router.post("/information")
+async def information(username: str):
+    if not is_exists_user(username):
+        return {"status:": False, "message": "User is not exits"}
+
+    information_query = {"username": username}
+    user_information = user_collection.find_one(information_query, {"_id": 0})
+    status = user_information != None
+    return {"status": status, "data": user_information}
 
 
 @router.post("/register")
