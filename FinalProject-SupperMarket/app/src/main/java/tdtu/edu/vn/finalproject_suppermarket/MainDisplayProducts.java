39c180bca0ba61c7893bcf78cfc43ad06d676a17
Thinh Vo <92027164+thinhvo1325@users.noreply.github.com>
2023-05-04 10:41:44 +0700

    feat: add login with gmail and facebook

--- FinalProject-SupperMarket/app/src/main/java/tdtu/edu/vn/finalproject_suppermarket/MainDisplayProducts.java
+++ FinalProject-SupperMarket/app/src/main/java/tdtu/edu/vn/finalproject_suppermarket/MainDisplayProducts.java
@@ -3,12 +3,52 @@ package tdtu.edu.vn.finalproject_suppermarket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainDisplayProducts extends AppCompatActivity {
 
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_display_products);
+
+        //Google Signin
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(googleSignInAccount!=null){
+            Toast.makeText(this, googleSignInAccount.getDisplayName() + "//"+googleSignInAccount.getEmail(), Toast.LENGTH_SHORT).show();
+        }
+
+        //Facebook Signin
+        /*AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken!=null && accessToken.isExpired()==false){
+            GraphRequest request = GraphRequest.newMeRequest(
+                    accessToken,
+                    new GraphRequest.GraphJSONObjectCallback() {
+                        @Override
+                        public void onCompleted(
+                                JSONObject object,
+                                GraphResponse response) {
+                            try {
+                                String fullName = object.getString("name");
+                                Toast.makeText(getApplicationContext(), fullName, Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,link");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }*/
     }
 }
